1aff7f6fa78383fc81f249c194f2cdce4c472d42
Jeremy Antoine <jeremy.antoine@antoinesolutions.com>
2023-03-22 09:38:53 -0500

    Added html, css, and js index pages.

---
+++ index.js
@@ -0,0 +1,7 @@
+function decrementVitality(source, value) {
+  document.getElementById(source).innerText = value - 1;
+}
+
+function incrementVitality(source, value) {
+  document.getElementById(source).innerText = value + 1;
+}
